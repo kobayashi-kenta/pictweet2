232ec23d764f708611fa66ec3072ec153548a9d8
kobayashi-kenta <12321klj@gmail.com>
2020-10-19 16:14:51 +0900

    完成

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,12 @@
 class Tweet < ApplicationRecord
   validates :text, presence: true
+  belongs_to :user
+  has_many :comments
+
+  def self.search(search)
+    if search != ""
+      Tweet.where('text LIKE(?)', "%#{search}")
+    else
+      Tweets.all
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_15_014127) do
+ActiveRecord::Schema.define(version: 2020_10_16_141603) do
+
+  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "tweet_id"
+    t.text "text"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
     t.string "text"
     t.text "image"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
